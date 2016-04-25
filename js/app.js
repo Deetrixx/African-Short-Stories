1e4401cbba9c7b1c35dfdc89a575a3ef0c305725
Deetrixx <dennisgithua30@gmail.com>
2016-04-25 09:58:43 +0300

    added a signup form to my index

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-$(document).foundation()
+$(document).ready(function () {
+    $('#name').focus();
+
+    var progressTriangle = $('#progress-triangle');
+
+    $('#name').focus(function () {
+        progressTriangle.animate({
+            top: "48px"
+        });
+    });
+    $('#email').focus(function () {
+        progressTriangle.animate({
+            top: "118px"
+        });
+    });
+    $('#password').focus(function () {
+        progressTriangle.animate({
+            top: "190px"
+        });
+    });
+    $('#select').focus(function () {
+        progressTriangle.animate({
+            top: "262px"
+        });
+    });
+});
